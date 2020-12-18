6ad64da7dfabc6a04813a9628c094734df6f7090
Daniel E. Cook <danielecook@gmail.com>
2020-12-18 13:38:21 -0500

    fix tests

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -21,13 +21,17 @@ assert_no_stdout
 run test_login gg sync --token ${TEST_TOKEN}
 assert_in_stderr "ggtest-2"
 
+stdin_test() {
+    cat db.go | gg new --description "test -- stdin"
+}
+
 # Create new gist - stdin
-#run test_new_stdin cat db.go | gg new --description "test -- stdin" db.go
-#assert_in_stderr https://gist.github.com/
+run test_new_stdin stdin_test
+assert_in_stdout https://gist.github.com/
 
 # Create new gist - filename
 run test_new_files gg new --description "test -- files" db.go README.md
-assert_in_stderr https://gist.github.com/
+assert_in_stdout https://gist.github.com/
 
 # Delete a gist
 run rm_gist gg rm 0
